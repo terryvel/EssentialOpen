69aa58daf0769b4bfd7e84e1a60491446a31acfb
terryaguiar <terryaguiar@suzano.com.br>
2026-02-22 00:45:55 -0300

    Added api to docker compose

--- viewer/startup.sh
+++ viewer/startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 echo "Starting SSH ..."
 service ssh start
-
-# Start oauth2-proxy in background
-nohup  /opt/oauth2-proxy --custom-sign-in-logo '-' --http-address 0.0.0.0:80 > /var/log/oauth2-proxy.log &
-
 catalina.sh run
